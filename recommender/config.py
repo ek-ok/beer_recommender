cef75104252cba1767f36a5e8d5a81636d3b5e20
Ikkei <ikkeiitoku@gmail.com>
2015-05-31 23:35:26 -0400

    Removed unnecessary files

--- recommender/config.py
+++ recommender/config.py
@@ -1,18 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+This defines config as objects
+"""
+
+
 class Config(object):
+    #Flask config
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///organise.db'
-    SECRET_KEY = 'something secret'
 
+    #DB config
+    DB_SERVER = 'aws-postgres01.colxr7rvrp2e.us-east-1.rds.amazonaws.com'
+    DB_USERNAME = 'user'
+    DB_PASSWORD = 'pass'
+    DB_DATABASE = 'beer'
+    DB_port = 5432
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql://user@localhost/foo'
-
+    pass
 
 class DevConfig(Config):
     DEBUG = True
-    SECRET_KEY = 'super secret?'
-
 
 class TestConfig(Config):
     TESTING = True
