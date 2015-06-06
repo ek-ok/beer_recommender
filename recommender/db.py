5e9f7b25416b889a1e8341cff48d5d32c5707750
Ikkei <ikkeiitoku@gmail.com>
2015-06-06 12:18:10 -0400

    added analysis

--- recommender/db.py
+++ recommender/db.py
@@ -10,15 +10,15 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 
-
 def coxn_str():
     server = app.config['DB_SERVER']
-    username = app.config['DB_SERVER']
-    password = app.config['DB_SERVER']
-    database = app.config['DB_SERVER']
+    username = app.config['DB_USERNAME']
+    password = app.config['DB_PASSWORD']
+    database = app.config['DB_DATABASE']
     port = app.config['DB_PORT']
 
-    return 'postgresql://%s:%s@%S:%s/%s' % (username, password, server, port, database)
+    return 'postgresql://%s:%s@%s:%s/%s' % (username, password, server, port, database)
+
 
 def fetch_data(query):
     engine = create_engine(coxn_str())
