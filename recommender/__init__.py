d7213b2727a1928a0eca3145232f81b2a63dfa66
Ikkei <ikkeiitoku@gmail.com>
2015-06-10 06:40:55 -0400

    preparing for eb deploy

--- recommender/__init__.py
+++ recommender/__init__.py
@@ -12,17 +12,17 @@ import os
 from recommender.config import ProdConfig
 from recommender.config import DevConfig
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 if os.uname()[1] == 'MacBookPro.local':
-    app.config.from_object(DevConfig)
+    application.config.from_object(DevConfig)
 else:
-    app.config.from_object(ProdConfig)
+    application.config.from_object(ProdConfig)
 
 from recommender.views.main import main
 from recommender.views.rec import rec
 from recommender.views.analysis import anal
 
-app.register_blueprint(main)
-app.register_blueprint(rec, url_prefix='/recommend')
-app.register_blueprint(anal, url_prefix='/analysis')
+application.register_blueprint(main)
+application.register_blueprint(rec, url_prefix='/recommend')
+application.register_blueprint(anal, url_prefix='/analysis')
