7430bda301045e5f7100f9874fa9f7a8c35d80e7
Ikkei <ikkeiitoku@gmail.com>
2015-06-13 21:25:02 -0400

    commiting config

--- recommender/config.py
+++ recommender/config.py
@@ -10,19 +10,23 @@ class Config(object):
     #Flask config
     DEBUG = False
     TESTING = False
+    PORT = 8080
 
     #DB config
     DB_SERVER = 'aws-postgres01.colxr7rvrp2e.us-east-1.rds.amazonaws.com'
-    DB_USERNAME = 'user'
-    DB_PASSWORD = 'pass'
+    DB_USERNAME = 'postgres_user01'
+    DB_PASSWORD = 'a1649100A'
     DB_DATABASE = 'beer'
-    DB_port = 5432
+    DB_PORT = 5432
+
 
 class ProdConfig(Config):
-    pass
+    PORT = 80
+
 
 class DevConfig(Config):
     DEBUG = True
+
 
 class TestConfig(Config):
     TESTING = True
