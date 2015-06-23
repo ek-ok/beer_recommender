fa8f59d19aa016d8d92e37494e7d52b1c8d802ab
Ikkei <ikkeiitoku@gmail.com>
2015-06-22 22:25:12 -0400

    adding testing

--- recommender/config.py
+++ recommender/config.py
@@ -18,7 +18,7 @@ class Config(object):
     DB_USERNAME = os.environ['DB_USERNAME']
     DB_PASSWORD = os.environ['DB_PASSWORD']
     DB_DATABASE = os.environ['DB_DATABASE']
-    DB_PORT = os.environ['DB_PORT']
+    DB_PORT = 5432
 
 
 class ProdConfig(Config):
