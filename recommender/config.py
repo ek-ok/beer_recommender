2ddea348023d5b6a75fb410200c83499f1443177
Ikkei <ikkeiitoku@gmail.com>
2015-06-22 21:22:42 -0400

    updated analysis

--- recommender/config.py
+++ recommender/config.py
@@ -13,11 +13,11 @@ class Config(object):
     PORT = 8080
 
     #DB config
-    DB_SERVER = 'aws-postgres01.colxr7rvrp2e.us-east-1.rds.amazonaws.com'
+    DB_SERVER = 'host'
     DB_USERNAME = 'user'
     DB_PASSWORD = 'pass'
     DB_DATABASE = 'beer'
-    DB_port = 5432
+    DB_PORT = 5432
 
 
 class ProdConfig(Config):
